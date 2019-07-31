452a7262e5f7075e0d6315cb6b9cda5c3b869f19
Marcelo <borgesnogueira@gmail.com>
2019-07-31 15:49:38 -0700

    Update mostra_cores_dos_peixes.m
    Corrigindo a exibição da cor achada para os n peixes (problema grado pela desontinuidade da representacao da cor h).

--- mostra_cores_dos_peixes.m
+++ mostra_cores_dos_peixes.m
@@ -16,6 +16,10 @@ function [] = mostra_cores_dos_peixes(media, variancia)
     for i=1:1:length(media) %numero de animais da primeira media (por que media é um vetor)
         x = linspace(media(i) - 3*sqrt(variancia(i)), media(i) + 3*sqrt(variancia(i)), dimY); %a linha das cores usando a media e a variancia
         
+        x(find(x<0)) = x(find(x<0)) + 1
+        
+        x(find(x>1)) = x(find(x>1)) - 1
+        
         for j=1:dimX
             imagemHSV(j,:,1) = x;
         end
