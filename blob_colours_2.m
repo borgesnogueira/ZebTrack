e79fc4ea7bd9d66748c088bf13203687030473be
Mateus de Assis <mateusassis@ufrn.edu.br>
2020-07-08 15:20:49 -0300

    plotting rgb added

--- blob_colours_2.m
+++ blob_colours_2.m
@@ -2,12 +2,19 @@
 
 function cor_atual = blob_colours_2(frame, l, c, cx, cy... 
                                   ,radius, boundingbox, ndetect...
-                                  , wframe_log, INTENSIDADE)
-                              
+                                  , wframe_log, saturation_threshold, value_threshold)
+% value_threshold == INTENSIDADE                              
 cor_atual = zeros(1,ndetect); %vetor com quantidade de espaços correspondentes as cores de cada animal.
-mediaFrameIndividual = 0; %(Em 1 peixe e muda em cada loop).                           
+mediaFrameIndividual = 0; %(Em 1 peixe e muda em cada loop).   
+how_many_pixels_were_considered = 0;
 
-somaRGB = zeros(ndetect,3)
+somaR = cast(zeros(ndetect,1),'uint8');
+somaG = cast(zeros(ndetect,1),'uint8');
+somaB = cast(zeros(ndetect,1),'uint8');
+
+valuesR = [];
+valuesG = [];
+valuesB = [];
 
 imshow(frame);
 hold on;
@@ -19,8 +26,19 @@ for k=1:1:ndetect %iterar sobre cada blob
        for j= round(boundingbox(k,2)):1:round(boundingbox(k,2))+round(boundingbox(k,4))
           if wframe_log(i,j) ~= 0
               pixelHSV = reshape(rgb2hsv(frame(i,j,:)),[1,3]);
-              if pixelHSV(1,3)>INTENSIDADE
-                  somaRGB(k,:) = somaRGB(k,:) + reshape(frame(i,j,:),[1,3]);
+              pixelRGB = reshape(frame(i,j,:),[1,3]);
+              if pixelHSV(1,3)>saturation_threshold
+                  how_many_pixels_were_considered = how_many_pixels_were_considered+1;
+                  somaR(k,1) = somaR(k,1) + pixelRGB(1);
+                  somaG(k,1) = somaG(k,1) + pixelRGB(2);                  
+                  somaB(k,1) = somaB(k,1) + pixelRGB(3);
+                  disp('soma RGB:');
+                  disp([somaR,somaG,somaB]);
+                  
+                  valuesR = [valuesR, pixelRGB(1)];
+                  valuesG = [valuesG, pixelRGB(2)];
+                  valuesB = [valuesB, pixelRGB(3)];
+                  
               end    
               %somaRGB(k,:) = somaRGB(k,:) + frame(i,j,:);
           end    
@@ -28,5 +46,16 @@ for k=1:1:ndetect %iterar sobre cada blob
     end
 %    disp(['for k = ',num2str(k),',somaRGB = ',num2str(somaRGB(1,k))])
 end
-somaRGB
+
+disp(['how many pixels? ', num2str(how_many_pixels_were_considered)])
+
+figure(2)
+plot(valuesR,'Color','r');
+hold on;
+plot(valuesG,'Color','g');
+hold on;
+plot(valuesB,'Color','b');
+
+disp(['mean(R)=', num2str(mean(valuesR)),'; mean(G)=',num2str(mean(valuesG)),'; mean(B)=',num2str(mean(valuesB))])
+
 end                             
