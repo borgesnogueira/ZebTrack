53736f6b28f1fba2e0217d3ff65166c59edc0436
Mateus de Assis <mateusassis@ufrn.edu.br>
2020-07-21 22:03:33 -0300

    threshold tests corrected

--- blob_colours_2.m
+++ blob_colours_2.m
@@ -2,9 +2,9 @@
 
 function avg_vector = blob_colours_2(frame, l, c, cx, cy... 
                                   ,radius, boundingbox, ndetect...
-                                  , wframe_log, value_threshold)
+                                  , wframe_log, value_threshold, saturation_threshold)
 % value_threshold == INTENSIDADE                              
-avg_vector = cell(ndetect,1) %cell array com quantidade de espaços correspondentes aos vetores de cor para cada animal.
+avg_vector = cell(ndetect,1); %cell array com quantidade de espaços correspondentes aos vetores de cor para cada animal.
 
 %lista_de_imagens_rgb = cell(ndetect,1);
 %lista_de_imagens_hsv = cell(ndetect,1);
@@ -19,24 +19,20 @@ for k=1:1:ndetect %iterar sobre cada blob
     imdif_retalho = imcrop(wframe_log, boundingbox(k,:));
     retalho_mascarado = bsxfun(@times,frame_retalho, cast(imdif_retalho,class(frame_retalho)));
     r_m_hsv = rgb2hsv(retalho_mascarado);
-    mask_value = r_m_hsv(:,:,3)>value_threshold;
+    mask_value = r_m_hsv(:,:,3)>=value_threshold & r_m_hsv(:,:,2)>= saturation_threshold;
     p_image = bsxfun(@times,retalho_mascarado, cast(mask_value,class(retalho_mascarado)));
     avg_p_i = reshape(sum(sum(p_image,1),2),[1,3]);
     how_many_pixels = sum(sum(imdif_retalho.*mask_value,1),2);
     if how_many_pixels ~= 0
-        avg_p_i = avg_p_i/how_many_pixels
+        avg_p_i = avg_p_i/how_many_pixels;
     end
     
-    avg_vector{k,1} = avg_p_i;
-    %{
-    lista_de_blobs{k,1} = imcrop(wframe_log,boundingbox(k,:));
-    lista_de_imagens_rgb{k,1} = imcrop(frame, boundingbox(k,:));
-    lista_de_imagens_hsv{k,1} = rgb2hsv(lista_de_imagens_rgb{k,1});
-    logical_mask = lista_de_imagens_hsv{k,1}(:,:,3)>value_threshold;
-    average_hue(1,k) = sum(sum(~logical_mask .*  lista_de_imagens_hsv{k,1}(:,:,1)),'omitnan');
-    figure(k);
-    imshow(lista_de_blobs{k,1});
-    %}
+    avg_vector{k,1} = avg_p_i;   
+    
+    %figure; %test only, remove later
+    %image(reshape(cast(avg_p_i,'uint8'),[1,1,3])); %test only, remove later
+
+
 end
-%disp(average_hue);
+
 end                             
