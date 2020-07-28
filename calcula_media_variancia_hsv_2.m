9ce55e0610fc080cc46b45e175dce6279005ea5f
Mateus de Assis <mateusassis@ufrn.edu.br>
2020-07-28 15:22:08 -0300

    code bones added

---
+++ calcula_media_variancia_hsv_2.m
@@ -0,0 +1,15 @@
+function [media, variancia] = calcula_media_variancia_hsv_2(video, tempo_inicial, tempo_final ...
+                                                       , Imback, V, nanimais, mascara, minpix, maxpix, tol, avi, criavideo, tipsubfundo ...
+                                                       , caixa, l, c ...
+                                                       , colorida, cor, tipfilt ...
+                                                       , INTENSIDADE)
+                                                   
+end
+
+
+%Função para converter meu tempo inicial e final em termos dos frames correspondentes.
+function [frame_inicial, frame_final] = extraiIntervaloFrames(tempo_inicial, tempo_final, video)
+    frame_inicial = video.FrameRate*tempo_inicial;
+    frame_final = video.FrameRate*tempo_final;  
+end
+
