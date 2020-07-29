f5b7be35448e901db46c17364a05e65cab40f39b
Mateus de Assis <mateusassis@ufrn.edu.br>
2020-07-28 21:56:41 -0300

    new_video line added

--- calcula_media_variancia_hsv_2.m
+++ calcula_media_variancia_hsv_2.m
@@ -1,14 +1,22 @@
+%{
+Para esse código, estou deliberadamente ignorando a dica
+%}
+
 function [media, variancia] = calcula_media_variancia_hsv_2(video, tempo_inicial, tempo_final ...
                                                        , Imback, V, nanimais, mascara, minpix, maxpix, tol, avi, criavideo, tipsubfundo ...
                                                        , caixa, l, c ...
                                                        , colorida, cor, tipfilt ...
                                                        , INTENSIDADE)
 
-    
+
     [frame_inicial, frame_final] = extraiIntervaloFrames(tempo_inicial, tempo_final, video); %aqui obtenho os índices final e inicial para a calibração.
-    
-    frames_video = read(video, floor([frame_inicial, frame_final]));                         %cria um vetor com todos os frames entre frame_incial e frame_final.
+    new_video = VideoReader([video.Path,'\',video.Name]); % preciso criar um novo VideoReader pra evitar um bug  
+    frames_video = read(new_video, floor([frame_inicial, frame_final]));                         %cria um vetor com todos os frames entre frame_incial e frame_final.
                                                                                              %Lembrando que para acessar o i-ésimo frame, uso a notação frames_video(:,:,:,i);                                                   
+    length_frames_video = (floor(frame_final) - floor(frame_inicial)) + 1;                   %Necessário para a implementação do for (o +1 é pra incluir o primeiro termo!)
+    %disp(['frame_inicial =', num2str(frame_inicial),'; frame_final = ',num2str(frame_final)]);                                                                                             
+    %media = 0;
+    %variancia = 1;
 end
 
 
