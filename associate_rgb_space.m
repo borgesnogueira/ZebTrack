c39da6c38ae9d94096f4cceea907e338ed835a76
Mateus de Assis <mateusassis@ufrn.edu.br>
2020-08-17 21:50:44 -0300

    starting to write

--- associate_rgb_space.m
+++ associate_rgb_space.m
@@ -1,18 +1,26 @@
 function centroids_index = associate_rgb_space(bc2_avg_vector,centroids)
-%{
-v1 = [1 2 3]; v2 = [1 2 0];
-dist = sqrt(sum((v1-v2).^2))
-%}
+    %{
+    v1 = [1 2 3]; v2 = [1 2 0];
+    dist = sqrt(sum((v1-v2).^2))
+    %}
 
-%{
-ideia do codigo: 
-aplica-se, ao frame em que os peixes vão ser rastreados, blob_colours_2. 
-receber o resultado de blob_colours_2.m (bc2_avg_vector)
-e um dos resultados da calcula_centroids_cov_rgb.m (centroids).
+    %{
+    ideia do codigo: 
+    aplica-se, ao frame em que os peixes vão ser rastreados, blob_colours_2. 
+    receber o resultado de blob_colours_2.m (bc2_avg_vector)
+    e um dos resultados da calcula_centroids_cov_rgb.m (centroids).
 
-o índice do centroide é atribuído a cada elemento do bc2_avg_vector. Isto
-é, se o primeiro RGB do bc2_avg_vector estiver mais perto do 2o centróide,
-o primeiro elemento do vetor centroids_index é 2.
-%}
-
+    o índice do centroide é atribuído a cada elemento do bc2_avg_vector. Isto
+    é, se o primeiro RGB do bc2_avg_vector estiver mais perto do 2o centróide,
+    o primeiro elemento do vetor centroids_index é 2.
+    %}
+    
+    [how_many_elements,~] = size(bc2_avg_vector);
+    centroids_index = zeros(how_many_elements,1);
+    mat_bc2_avg_v = cell2mat(bc2_avg_vector);
+    
+    D = pdist2(mat_bc2_avg_v, centroids,'Euclidean','Smallest',1);
+    
+    disp(D);
+    
 end
