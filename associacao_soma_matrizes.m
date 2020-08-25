feb904e211dd796a2b5da502ba2bd284917bbcef
Mateus de Assis <mateusassis@ufrn.edu.br>
2020-08-25 20:41:37 -0300

    nightly build, pxn and pyn arbitrarily set

--- associacao_soma_matrizes.m
+++ associacao_soma_matrizes.m
@@ -1,4 +1,4 @@
-function [pxn,pyn] = associacao_soma_matrizes()
+function [pxn,pyn] = associacao_soma_matrizes(bc2_avg_vector,centroids, cx, cy, px_ant, py_ant)
 %{
 note que existem nanimais. Isso que define a quantidade de centroides 
 (existem, por definição, a mesma quantidade de centróides que de animais).
@@ -7,13 +7,9 @@ questão.
 Logo, é possível definir duas matrizes, ambas de formato qtd_blobs x
 nanimais (qtd_centroids), tanto no espaço de cores quanto no espaço da imagem.
 Como definir essas matrizes?
+Vale saber que [cx cy] são os centróides dos blobs.
 
-o primeiro passo é o seguinte:
-
->> centro_de_blobs = boundingbox(:,1:2) +0.5*boundingbox(:,3:4)
-
-isso fará com que tenhamos os centros dos blobs (dos quadrados)
-o segundo passo é captar os pontos passados:
+o primeiro passo é captar os pontos passados:
 
 >> px_antes = px(:,cont-1); py_antes = py(:,cont-1);
 
@@ -30,6 +26,16 @@ daí basta somar:
 
 >> D = D_cores + D_imagem
 --------------
-
 %}
+
+    mat_bc2_avg_v = cell2mat(bc2_avg_vector);
+    D_cores = pdist2(mat_bc2_avg_v, centroids);
+    
+    centroides_boundingbox = [cx' cy'];
+    pontos_imagem = [px_ant py_ant];
+    D_imagem = pdist2(centroides_boundingbox, pontos_imagem);
+    pxn = 3
+    pyn = 2
+    disp(D_cores);
+    disp(D_imagem);
 end
