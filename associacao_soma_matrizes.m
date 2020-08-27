9496d5ce6a3d386d1cc936ca1c7b3d0255c58d73
Mateus de Assis Silva <mateusassis@ufrn.edu.br>
2020-08-27 19:19:17 -0300

    changed [cx' cy'] to [cx cy]

--- associacao_soma_matrizes.m
+++ associacao_soma_matrizes.m
@@ -1,19 +1,19 @@
 function [pxn,pyn] = associacao_soma_matrizes(bc2_avg_vector,centroids, cx, cy, px_ant, py_ant)
 %{
 note que existem nanimais. Isso que define a quantidade de centroides 
-(existem, por definição, a mesma quantidade de centróides que de animais).
-Ao mesmo tempo, a quantidade de blobs é uma característica do frame em
-questão.
-Logo, é possível definir duas matrizes de distancia, ambas de formato qtd_blobs x
-nanimais (qtd_centroids), tanto no espaço de cores quanto no espaço da imagem.
+(existem, por definiÃ§Ã£o, a mesma quantidade de centrÃ³ides que de animais).
+Ao mesmo tempo, a quantidade de blobs Ã© uma caracterÃ­stica do frame em
+questÃ£o.
+Logo, Ã© possÃ­vel definir duas matrizes de distancia, ambas de formato qtd_blobs x
+nanimais (qtd_centroids), tanto no espaÃ§o de cores quanto no espaÃ§o da imagem.
 Como definir essas matrizes?
-Vale saber que [cx cy] são os centróides dos blobs.
+Vale saber que [cx cy] sÃ£o os centrÃ³ides dos blobs.
 
-o primeiro passo é captar os pontos passados:
+o primeiro passo Ã© captar os pontos passados:
 
 >> px_antes = px(:,cont-1); py_antes = py(:,cont-1);
 
-e colocá-los numa matriz:
+e colocÃ¡-los numa matriz:
 
 >> mat_pxs = [px_antes py_antes];
 
@@ -22,7 +22,7 @@ agora define-se as matrizes:
 >> D_cores = pdist2(mat_bc2_avg_v, centroids); 
 >> D_imagem = pdist2(centro_de_blobs, mat_pxs);
 
-daí basta somar:
+daÃ­ basta somar:
 
 >> D = D_cores + D_imagem
 --------------
@@ -31,7 +31,7 @@ daí basta somar:
     mat_bc2_avg_v = cell2mat(bc2_avg_vector);
     D_cores = pdist2(mat_bc2_avg_v, centroids);
     
-    centroides_boundingbox = [cx' cy'];
+    centroides_boundingbox = [cx cy];
     pontos_anteriores_imagem = [px_ant py_ant];
     D_imagem = pdist2(centroides_boundingbox, pontos_anteriores_imagem);
    % disp(D_cores);
