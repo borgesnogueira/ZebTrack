3fe944e548c070170e6206693c80c5edaa8631bd
Marcelo <borgesnogueira@gmail.com>
2020-10-20 19:05:47 -0300

    vetor de cores vazio
    se nao achar pixels coloridos, retorna vetro de cores vazio

--- blob_colours_2.m
+++ blob_colours_2.m
@@ -24,10 +24,10 @@ for k=1:1:ndetect %iterar sobre cada blob
     how_many_pixels = sum(sum(imdif_retalho.*mask_value,1),2);
     if how_many_pixels ~= 0
         avg_p_i = avg_p_i/how_many_pixels;
+        avg_vector{k,1} = avg_p_i;   
     else
         disp('no valid pixels found');
     end
-    avg_vector{k,1} = avg_p_i;   
     
     %figure; %test only, remove later
     %image(reshape(cast(avg_p_i,'uint8'),[1,1,3])); %test only, remove later
